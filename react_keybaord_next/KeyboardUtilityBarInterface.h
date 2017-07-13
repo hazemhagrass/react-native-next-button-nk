b38c9473cca53ea986b9161bfb70113e7edbd01e
Mohannad A. Hassan <mohannad.hassan@badrit.com>
2017-07-13 02:38:02 +0200

    Create KeyboardUtilityBarInterface to be the interface declaring React's interface

---
+++ react_keybaord_next/KeyboardUtilityBarInterface.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardUtilityBarInterface.h
+//  react_keybaord_next
+//
+//  Created by Mohannad A. Hassan on 7/13/17.
+//  Copyright © 2017 Mohannad A. Hassan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KeyboardUtilityBarInterface : NSObject
+
+@end
---
+++ react_keybaord_next/KeyboardUtilityBarInterface.m
@@ -0,0 +1,13 @@
+//
+//  KeyboardUtilityBarInterface.m
+//  react_keybaord_next
+//
+//  Created by Mohannad A. Hassan on 7/13/17.
+//  Copyright © 2017 Mohannad A. Hassan. All rights reserved.
+//
+
+#import "KeyboardUtilityBarInterface.h"
+
+@implementation KeyboardUtilityBarInterface
+
+@end
